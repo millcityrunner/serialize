add5b40897338aa78decad49245b080f5dcd89cd
millcityrunner <andrew.ray2@target.com>
2020-04-27 13:36:15 -0500

    adding in all of the defaults and test suite

---
+++ examples/extra_params_ex.py
@@ -0,0 +1,51 @@
+from serial_j import SerialJ
+
+class OptionalArgs(SerialJ):
+    # the `optional`, `nullable`, and `default` are optional parameters
+    # `optional` -> defaults to False, if True this parameter is not required to be passed into the serializer
+    # `nullable` -> defaults to False, if True this parameter can be passed in as a `null` equivalent value
+    # `default` -> defaults to None, this parameter only applies if the key is not present in the data,
+    #                  cannot apply a default value to a key that is defined to NOT be optional
+    schema = [
+        {'name': 'player_1', 'type': (str,), 'optional': False, 'nullable': False, 'default': None},
+        {'name': 'player_2', 'type': (str,), 'optional': True, 'nullable': False, 'default': 'Player 2'},
+    ]
+
+
+class CannotBeDefaulted(SerialJ):
+    schema = [
+        {'name': 'player_1', 'type': (str,), 'optional': False, 'nullable': False, 'default': 'Player 1'}
+    ]
+
+
+if __name__ == '__main__':
+    # here is what will happen if only player_1 is passed in
+    test_data = {
+        'player_1': 'Alan Turing'
+    }
+
+    s1 = OptionalArgs(data=test_data)
+    print(s1)
+    # >>> s = {"player_1": "Alan Turing", "player_2": "Player 2"}
+
+    # here is what will happen if both values are passed in
+    test_data_2 = {
+        'player_1': 'Alan Turing',
+        'player_2': 'Steve Jobs'
+    }
+
+    s2 = OptionalArgs(data=test_data_2)
+    print(s2)
+    # >>> s = {"player_1": "Alan Turing", "player_2": "Steve Jobs"}
+
+    # here is what will happen if `default` is passed in on a non-optional key
+    s3 = CannotBeDefaulted(data=test_data)
+    print(s3)
+    # >>>   Traceback (most recent call last):
+    #           File "/serial-j/examples/extra_params_ex.py", line 41, in <module>
+    #               s3 = CannotBeDefaulted(data=test_data)
+    #           File "/serial-j/serial_j/__init__.py", line 15, in __init__
+    #               self.preproc(data)
+    #           File "/serial-j/serial_j/__init__.py", line 85, in preproc
+    #               raise ValueError(_err(6, _name))
+    #       ValueError: Property: 'player_1' cannot have a default value when it is required.
--- serial_j/__init__.py
+++ serial_j/__init__.py
@@ -3,7 +3,7 @@ from types import LambdaType
 from uuid import UUID
 from serial_j.hp import _valid_regex, _regex_match, _err, _spsstps
 from serial_j.const import _spbtps, _na, _nu, _tp, _opt, _cp, _srl, \
-    _sch, _empt
+    _sch, _empt, _def
 
 name = "serial_j"
 
@@ -12,34 +12,34 @@ class SerialJ(object):
     schema = []
 
     def __init__(self, data):
-        self._preproc(data)
-        self._proc(data)
+        self.preproc(data)
+        self.proc(data)
 
     @staticmethod
-    def _valid_type(_type):
+    def valid_type(_type):
         if isinstance(_type, tuple) and len(_type) == 2:
             mt = _type[0]
             st = _type[1]
             if mt not in _spbtps:
                 return False
-            if (mt == int and not isinstance(st, tuple)
-                    and not isinstance(st, range)
-                    and not isinstance(st, LambdaType)):
+            if (mt == int and isinstance(st, tuple) is False
+                    and isinstance(st, range) is False
+                    and isinstance(st, LambdaType) is False):
                 return False
-            if mt == int and isinstance(st, tuple):
+            if mt == int and isinstance(st, tuple) is True:
                 for e in st:
-                    if not isinstance(e, int):
+                    if isinstance(e, int) is False:
                         return False
-            if (mt == str and not isinstance(st, tuple)
+            if (mt == str and isinstance(st, tuple) is False
                     and st not in _spsstps.keys()
-                    and not _valid_regex(st)):
+                    and _valid_regex(st) is False):
                 return False
-            if mt == str and isinstance(st, tuple):
+            if mt == str and isinstance(st, tuple) is True:
                 for e in st:
-                    if not isinstance(e, str):
+                    if isinstance(e, str) is False:
                         return False
             return True
-        elif isinstance(_type, tuple) and len(_type) == 1:
+        elif isinstance(_type, tuple) is True and len(_type) == 1:
             mt = _type[0]
             if mt not in _spbtps:
                 return False
@@ -48,51 +48,80 @@ class SerialJ(object):
             return False
 
     @staticmethod
-    def _validated(_type, data):
-        if isinstance(_type, tuple) and len(_type) == 2:
+    def validated(_type, data):
+        if isinstance(_type, tuple) is True and len(_type) == 2:
             mt = _type[0]
             st = _type[1]
-            if mt == int and (isinstance(st, tuple) or isinstance(st, range)):
+            if mt == int and (isinstance(st, tuple) is True or isinstance(st, range) is True):
                 return data in st
-            elif mt == int and isinstance(st, LambdaType):
+            elif mt == int and isinstance(st, LambdaType) is True:
                 return st(data)
-            elif mt == str and isinstance(st, tuple):
+            elif mt == str and isinstance(st, tuple) is True:
                 return data in st
             elif mt == str and st in _spsstps.keys():
                 return _spsstps[st](data)
             else:
                 return _regex_match(regex=st, _str=data)
-        elif isinstance(_type, tuple) and len(_type) == 1:
+        elif isinstance(_type, tuple) is True and len(_type) == 1:
             mt = _type[0]
             return isinstance(data, mt)
         else:
             return False
 
-    def _preproc(self, data):
+    def preproc(self, data):
         for prop in self.schema:
             _name = prop[_na]
             _type = prop[_tp] if _tp in prop else None
             _optional = prop[_opt] if _opt in prop else False
+            if isinstance(_optional, bool) is False:
+                raise TypeError(_err(8, _name=_opt, _type=type(_optional), _expected=bool))
             _nullable = prop[_nu] if _nu in prop else False
+            if isinstance(_nullable, bool) is False:
+                raise TypeError(_err(8, _name=_nu, _type=type(_nullable), _expected=bool))
             _compound = prop[_cp] if _cp in prop else False
+            if isinstance(_compound, bool) is False:
+                raise TypeError(_err(8, _name=_cp, _type=type(_compound), _expected=bool))
+
             _serializer = prop[_srl] if _srl in prop else None
             _schema = prop[_sch] if _sch in prop else None
-            if _type and not self._valid_type(_type):
+
+            # _default -> 'value', 1, (), [], or {}
+            _default = prop[_def] if _def in prop else None
+
+            if _type is True and self.valid_type(_type) is False:
                 raise TypeError(_err(5, _name, _type))
-            if not _optional and _name not in data:
+            if _default is True and isinstance(_default, _type) is False:
+                raise TypeError(_err(7, _name, _type, _default))
+            if _optional is False and _default is True:
+                raise ValueError(_err(6, _name))
+            if _optional is False and _name not in data:
                 raise ValueError(_err(0, _name, data))
             if _name in data:
                 if data[_name] in _empt:
-                    if not _nullable:
+                    if _nullable is False and _default is None and _optional is False:
                         raise ValueError(_err(1, _name))
                 else:
-                    if _type and not self._validated(_type, data[_name]):
+                    validated = self.validated(_type, data[_name])
+                    if _type is not None and validated is False:
                         raise ValueError(_err(4, _name, _type, data[_name]))
-                    if (_compound and not isinstance(data[_name], list)
-                            and not isinstance(data[_name], dict)):
+                    if (_compound is True and isinstance(data[_name], list) is False
+                            and isinstance(data[_name], dict) is False):
                         raise TypeError(_err(2, _name))
-                    if _compound and not _serializer and not _schema:
+                    if _compound is True and _serializer is False and _schema is False:
                         raise TypeError(_err(3, _name))
+
+            else:
+                if _default is None and _optional is False:
+                    raise ValueError(_err(1, _name))
+
+                elif _default is not None:
+                    if type(_default) != bool and isinstance(_default, _type[0]) is False:
+                        raise ValueError(_err(7, _name, _type=_type, _default=_default))
+                    elif type(_default) == bool != _type[0]:
+                        raise ValueError(_err(7, _name, _type=_type, _default=_default))
+                    elif isinstance(_default, _type[0]) is True and self.validated(_type, _default) is False:
+                        raise ValueError(_err(7, _name, _type=_type, _default=_default))
+
             prop[_na] = _name
             prop[_tp] = _type
             prop[_opt] = _optional
@@ -100,10 +129,13 @@ class SerialJ(object):
             prop[_cp] = _compound
             prop[_srl] = _serializer
             prop[_sch] = _schema
+            prop[_def] = _default
 
-    def _proc(self, data):
+    def proc(self, data):
         for prop in self.schema:
             _name = prop[_na]
+            _default = prop[_def]
+
             if _name in data:
                 if prop[_cp]:
                     if prop[_srl]:
@@ -125,6 +157,10 @@ class SerialJ(object):
                         self.__dict__[_name] = str(data[_name])
                     else:
                         self.__dict__[_name] = data[_name]
+
+            else:
+                if _default:
+                    self.__dict__[_name] = _default
 
     def as_dict(self):
         _d = {}
@@ -148,3 +184,14 @@ class SerialJ(object):
 
     def __repr__(self) -> str:
         return self._to_str()
+
+
+if __name__ == '__main__':
+    class TestRun(SerialJ):
+        schema = [
+            {'name': 'andrew', 'type': (int,)}
+        ]
+
+    test_data = {'andrew': 'hi'}
+    s = TestRun(data=test_data)
+
