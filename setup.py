6788990de7f7f57af8373cacf1600841edb2cc00
millcityrunner <andrew.ray2@target.com>
2020-04-28 15:16:53 -0500

    fixing artifactory deploys

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 import setuptools
 
-with open("Pypi.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='serial-j',
     version='1.1.4',
-    author='Junpu Fan - Original, Andrew Ray - Maintainer',
+    author='Andrew Ray',
     author_email='andrew.ray2@target.com',
-    description='Validating and Serializing JSON data into Python object with minimal effort.',
+    description='Creating a serializer for data structures',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://git.target.com/InfraServices-Storage/serial-j',
     license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'flask-sqlalchemy',
+        'sqlalchemy'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
